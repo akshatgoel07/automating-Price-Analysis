ac9623412df3c29a828fadf98734c78ef6e2303a
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2023-04-09 15:27:55 +0530

    script updated

--- ve/main.py
+++ ve/main.py
@@ -2,32 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(executable_path='C:/path/to/chromedriver.exe')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.flipkart.com")
+driver.find_element(By.XPATH, "/html/body/div[2]/div/div/button").click()
+time.sleep(5)
 search_bar = driver.find_element(By.CSS_SELECTOR, "._3704LK")
-search_bar.send_keys("sony headphones whch720phones")
+search_bar.send_keys("sony whch720n")
 search_bar.send_keys(Keys.ENTER)
+time.sleep(5)
+headphone = driver.find_element(By.CSS_SELECTOR,"._30jeq3")
+print( "Flipkart" ,headphone.text)
 
-time.sleep(5) # wait for the search results to load
-# flipkart_price = driver.find_element(By.CSS_SELECTOR, "._1vC4OE._2rQ-NK").text  
-# //*[@id="container"]/div/div[3]/div[1]/div[2]/div[2]/div/div[4]/div[1]/div/div[1]
-
-# flipkart_price_element = driver.find_element(By.XPATH, "//div[contains(@class, '_1vC4OE') and contains(@class, '_2rQ-NK')]")
-# flipkart_price = flipkart_price_element.text
+time.sleep(5)
 
 
-driver = webdriver.Chrome(executable_path='C:/path/to/chromedriver.exe')
 driver.get("https://www.amazon.in")
 search_bar = driver.find_element(By.ID, "twotabsearchtextbox")
-search_bar.send_keys("sony headphones whch720phones")
+search_bar.send_keys("sony whch720n")
 search_bar.send_keys(Keys.ENTER)
-
-
-
-time.sleep(20)
-amazon_price = driver.find_element(By.CSS_SELECTOR, ".s-result-item:nth-child(1) .a-offscreen").text
-print(amazon_price)
+headphone = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div[3]/div[1]/div/div[1]/div[1]/a/span/span[2]/span[2]")
+print("Amazon",headphone.text)
 
 
 
