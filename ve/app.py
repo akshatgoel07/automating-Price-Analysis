f29d9cdf019195af5fff95554a1f5845b67422e6
dhairyamarwah <dhairyamarwah01@gmail.com>
2023-04-11 12:16:28 +0530

    update

--- ve/app.py
+++ ve/app.py
@@ -1,53 +1,68 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 from selenium import webdriver
+from flask import jsonify
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+CORS(app)
 
 @app.route('/search', methods=['POST'])
-def search():
-    query = request.form['query']
-
+def search_headphone():
+    search_query = request.json['data']['query']
+    # Selenium code to search for headphone on Flipkart and Amazon
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://www.flipkart.com")
     driver.find_element(By.XPATH, "/html/body/div[2]/div/div/button").click()
     time.sleep(5)
     search_bar = driver.find_element(By.CSS_SELECTOR, "._3704LK")
-    search_bar.send_keys(query)
+    search_bar.send_keys(search_query)
     search_bar.send_keys(Keys.ENTER)
     time.sleep(5)
+    flipkart_product_name = driver.find_element(By.CSS_SELECTOR, ".s1Q9rs").text
     flipkart_headphone = driver.find_element(By.CSS_SELECTOR,"._30jeq3")
-    flipkart_price = flipkart_headphone.text.replace('₹','').replace(',','')
-    flipkart_model = flipkart_headphone.get_attribute('title')
+    flipkart_price = int(flipkart_headphone.text.replace('₹','').replace(',',''))
+    flipkart_link = driver.find_element(By.CSS_SELECTOR, '._2rpwqI').get_attribute('href')
+    flipkart_product_image = driver.find_element(By.CSS_SELECTOR, "._396cs4").get_attribute('src')
 
     time.sleep(5)
 
     driver.get("https://www.amazon.in")
     search_bar = driver.find_element(By.ID, "twotabsearchtextbox")
-    search_bar.send_keys(query)
+    search_bar.send_keys(search_query)
     search_bar.send_keys(Keys.ENTER)
-    time.sleep(5)
+    amazon_product_name = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span").text
     amazon_headphone = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div[3]/div[1]/div/div[1]/div[1]/a/span/span[2]/span[2]")
-    amazon_price = amazon_headphone.text.replace('₹','').replace(',','')
-    amazon_model = amazon_headphone.get_attribute('aria-label')
+    amazon_price = int(amazon_headphone.text.replace('₹','').replace(',',''))
+    amazon_link = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2/a").get_attribute('href')
+    amazon_product_image = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img").get_attribute('src')
 
     driver.quit()
 
-    if int(amazon_price) < int(flipkart_price):
-        better_price = "Amazon"
-    else:
-        better_price = "Flipkart"
-
-    return render_template('results.html', query=query, flipkart_price=flipkart_price, flipkart_model=flipkart_model, amazon_price=amazon_price, amazon_model=amazon_model, better_price=better_price)
-
+    response =[
+        {   
+        'company': 'Flipkart',
+        'product_name': flipkart_product_name,
+        'price': flipkart_price,
+        'link': flipkart_link,
+        'product_image': flipkart_product_image
+        },
+        {
+        'company': 'Amazon',
+        'product_name': amazon_product_name,
+        'price': amazon_price,
+        'link': amazon_link,
+        'product_image': amazon_product_image
+        },
+    ]
+    sorted_response = sorted(response, key=lambda k: k['price'])
+    return jsonify(sorted_response)
 
 if __name__ == '__main__':
     app.run()
